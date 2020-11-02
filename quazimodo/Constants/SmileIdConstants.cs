07aecbe446c7a787b0d0afe224af80aa39f0ef5f
Dmitry Karpovich <karpovichdi@hotmail.com>
2020-11-03 01:16:16 +0300

    Added: Icons for positive smiles.

--- quazimodo/Constants/SmileIdConstants.cs
+++ quazimodo/Constants/SmileIdConstants.cs
@@ -2,6 +2,50 @@
 {
     public static class SmileIdConstants
     {
-        public static string Kiss => "kiss.png";
+        public static string Alien001 => "alien.png";
+        public static string BlowKiss009 => "blowkiss.png";
+        public static string Kiss011 => "kiss.png";
+        public static string Closed014 => "closedeyes.png";
+        public static string Cool020 => "cool.png";
+        public static string Money027 => "money.png";
+        public static string Dollar028 => "dollar.png";
+        public static string Glance039 => "glanceq.png";
+        public static string GrandFather040 => "grandfather.png";
+        public static string Laughter042 => "Laughter.png";
+        public static string Grinning043 => "grinningq.png";
+        public static string Grinning044 => "grinningw.png";
+        public static string Grinning046 => "grinninge.png";
+        public static string Grinning047 => "grinningr.png";
+        public static string Grinning049 => "grinningy.png";
+        public static string Wink052 => "wink.png";
+        public static string Sweating053 => "sweating.png";
+        public static string Hand055 => "hand.png";
+        public static string HeartEyes056 => "hearteyes.png";
+        public static string HeartEyes057 => "hearteyesq.png";
+        public static string Hugging058 => "hugging.png";
+        public static string Kissing059 => "kissing.png";
+        public static string Laughing061 => "laughing.png";
+        public static string Policeman067 => "policeman.png";
+        public static string EyePatch075 => "eyepatch.png";
+        public static string Laughter079 => "Laughterq.png";
+        public static string Lol080 => "lol.png";
+        public static string Tasty086 => "tasty.png";
+        public static string SmillingFace095 => "smilingface.png";
+        public static string Devil098 => "devil.png";
+        public static string Smile100 => "smile.png";
+        public static string Angel102 => "angel.png";
+        public static string Hearts103 => "hearts.png";
+        public static string Sunglasses104 => "sunglasses.png";
+        public static string TongueOut109 => "tongueout.png";
+        public static string Starred111 => "starred.png";
+        public static string Sunglasses113 => "sunglassesq.png";
+        public static string Joy117 => "joy.png";
+        public static string Thief120 => "thief.png";
+        public static string Tease123 => "tease.png";
+        public static string UpsideDown128 => "upsidedown.png";
+        public static string WinkingFace132 => "winkingface.png";
+        public static string Yummy136 => "yummy.png";
+        public static string Happy137 => "happy.png";
+        public static string Zany138 => "zany.png";
     }
 }
