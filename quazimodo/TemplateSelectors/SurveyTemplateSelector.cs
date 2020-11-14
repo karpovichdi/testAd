f9b15915bdcc26f16e56425b3915097583fc4b9d
Dmitry Karpovich <karpovichdi@hotmail.com>
2020-11-14 22:35:19 +0300

    Fixed: Bug with launching.

---
+++ quazimodo/TemplateSelectors/SurveyTemplateSelector.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace quazimodo.TemplateSelectors
+{
+    public class SurveyTemplateSelector : DataTemplateSelector
+    {
+        private readonly DataTemplate _linearTemplate;
+        private readonly DataTemplate _tableTemplate;
+
+        public SurveyTemplateSelector()
+        {
+            // _linearTemplate = new DataTemplate(typeof(LinearView));    
+            // _tableTemplate = new DataTemplate(typeof(ProductsView));    
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            // return item is LinearSurveyViewModel ? _linearTemplate : _tableTemplate;
+            return null;
+        }
+    }
+}
